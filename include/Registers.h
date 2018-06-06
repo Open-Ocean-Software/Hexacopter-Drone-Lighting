0f4a686c3c96b73a9e214149a2d2cf830bb77613
Nicholas Hein <nichh81999@gmail.com>
2018-06-05 23:59:15 -0500

    Added Registers and Code Style
    Added current registers and styled the code.

---
+++ include/Registers.h
@@ -0,0 +1,20 @@
+
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+#include "Register.h"
+
+///////////////////
+// Add Registers //
+///////////////////
+#include "Registers/Control.h"
+#include "Registers/Preset.h"
+#include "Registers/LightStrips.h"
+#include "Registers/CameraFloodlight.h"
+#include "Registers/LandingFloodlight.h"
+#include "Registers/CameraPointLaser.h"
+#include "Registers/CameraLevelLaser.h"
+#include "Registers/ScanLaser.h"
+#include "Registers/StatusNotify.h"
+
+#endif
