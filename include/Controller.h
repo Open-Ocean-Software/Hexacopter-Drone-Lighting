51fcac912cd652f2cf530f6481be084cfbcd55e4
Nicholas Hein <nichh81999@gmail.com>
2018-06-10 15:29:30 -0500

    Added controller functionality

---
+++ include/Controller.h
@@ -0,0 +1,13 @@
+
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+void Activity (void);
+void Standby (void);
+
+void InitializeStopwatch (void);
+void StopwatchHandler (void);
+double ReadStopwatch (void);
+void ResetStopwatch (void);
+
+#endif
