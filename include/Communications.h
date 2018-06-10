5efbaf2e44cacba30a3c273d0a097f6f70be7030
Nicholas Hein <nichh81999@gmail.com>
2018-06-10 15:51:38 -0500

    Moved config macros to Config.h

--- include/Communications.h
+++ include/Communications.h
@@ -2,9 +2,6 @@
 #ifndef COMMUNICATIONS_H
 #define COMMUNICATIONS_H
 
-#define READMODE 0x0
-#define WRITEMODE 0x80
-
 unsigned char ReadCommunications (void);
 unsigned char ReadCommunications (struct Register *reg);
 unsigned char WriteCommunications (unsigned char *val);
