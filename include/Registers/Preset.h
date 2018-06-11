6800a2adb0094cc4ebbe84173642d101c04de219
Nicholas Hein <nichh81999@gmail.com>
2018-06-11 07:59:00 -0500

    Integrated config header

--- include/Registers/Preset.h
+++ include/Registers/Preset.h
@@ -2,6 +2,8 @@
 #ifndef R_PRESET_H
 #define R_PRESET_H
 
+#include "../Config.h"
+
 #define REGID_PRESET 0x02
 #define REG_PRESET_DEFAULT 0x00
 #define Reg_Preset_Get() (RegisterGet(REGID_PRESET))
@@ -10,7 +12,7 @@
 #define Reg_Preset_SetValue(v) (RegisterSetValue(REGID_PRESET, v))
 #define Reg_Preset_Reset() (RegisterSet(REGID_PRESET, REG_PRESET_DEFAULT))
 
-#define REG_PRESET_FLAG_PERSIST 0x80
+#define REG_PRESET_FLAG_PERSIST CONFIG_PRESETS_PERSISTMASK
 #define Reg_Preset_GetPersist() (RegisterFlagGetValue(REGID_PRESET, REG_PRESET_FLAG_PERSIST) != 0x0)
 #define Reg_Preset_SetPersist(e) (RegisterFlagSet(REGID_PRESET, REG_PRESET_FLAG_PERSIST, (e != 0x0)))
 #define Reg_Preset_Toggle_Persist() (RegisterFlagToggle(REGID_PRESET, REG_PRESET_FLAG_PERSIST))
